2cc122fe080ac52c8e560b437a88d4ec848fe717
willians-wil <wnarcizzo@gmail.com>
2025-10-03 21:34:39 -0300

    Excluindo o espaço que havia no button e alterando o nome product na chamada do scripts

--- arquivo.js
+++ arquivo.js
@@ -4,23 +4,24 @@ const buttonDiscount = document.querySelector(".show-descont");
 const itemDescont = document.querySelector(".total-sum");
 const productVegan = document.querySelector(".vegan-product");
 
+// Reaproveitável para mostrar produtos dinamicamente
 function showAll(productArray) {
   let myLi = "";
 
   productArray.forEach((product) => {
     myLi += `
-             <li>
-                    <img src= ${product.src}>
-                    <p>${product.name}</p>
-                    <p class="item-price">R$ ${product.price}</p> 
-             </li>
-          
-             `;
+      <li>
+        <img src="${product.src}" alt="${product.name}" />
+        <p>${product.name}</p>
+        <p class="item-price">R$ ${product.price.toFixed(2)}</p>
+      </li>
+    `;
   });
+
   list.innerHTML = myLi;
-  e;
 }
 
+// Mostrar produtos com desconto (10%)
 function listDescont() {
   const newPrice = menuOptions.map((product) => ({
     ...product,
@@ -30,37 +31,24 @@ function listDescont() {
   showAll(newPrice);
 }
 
+// Somar o valor total dos produtos
 function sumOfitens() {
-  const newDescont = menuOptions.reduce((acc, value) => acc + value.price, 0);
-  list.innerHTML = ` 
-       <li>
-              <p>O valor total dos itens é = ${newDescont}</p>
-       </li>
-       
-       `;
+  const total = menuOptions.reduce((acc, item) => acc + item.price, 0);
 
-  console.log(newDescont);
+  list.innerHTML = `
+    <li>
+      <p>O valor total dos itens é: R$ ${total.toFixed(2)}</p>
+    </li>
+  `;
 }
 
+// Filtrar e mostrar só os veganos
 function listVegan() {
-  const listVegan = menuOptions.filter((vegan) => {
-    list.innerHTML = `
-              <li>
-                     <img src="./assets/xvegan.png" alt="Burger-Vegano" />
-                     <p>Big Vegano</p>
-                     <p class="item-price">R$ 55,00</p>
-              </li>    
-              
-              <li>
-                     <img src="./assets/monstruoso-vegan.png" alt="Burger-X-Vegan" />
-                     <p>X-Vegan</p>
-                     <p class="item-price">R$ 45,00</p> 
-              </li>
-              
-              `;
-  });
+  const veganItems = menuOptions.filter((item) => item.vegan === true);
+  showAll(veganItems);
 }
 
+// Eventos dos botões
 buttonShowall.addEventListener("click", () => showAll(menuOptions));
 buttonDiscount.addEventListener("click", listDescont);
 itemDescont.addEventListener("click", sumOfitens);
